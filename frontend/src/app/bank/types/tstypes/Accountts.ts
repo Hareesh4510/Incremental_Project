648f5f687d0596bc125eea9c5a3eb261fbe285c2
username <wecp-dummy@gmail.com>
2024-07-24 12:10:38 +0000

    Day16Task

--- frontend/src/app/bank/types/tstypes/Accountts.ts
+++ frontend/src/app/bank/types/tstypes/Accountts.ts
@@ -1,4 +1,3 @@
-//Accountts.ts
 export class AccountTS {
     accountId?: string;
     customerId: string;
@@ -8,13 +7,13 @@ export class AccountTS {
         this.customerId = customerId;
         this.balance = balance;
     }
-
+ 
     displayInfo() {
         console.log(`Account ID: ${this.accountId}`);
         console.log(`Customer ID: ${this.customerId}`);
         console.log(`Balance: ${this.balance.toFixed(2)}`);
     }
 }
-
+ 
 const account = new AccountTS("1",1000.00,"1");
 account.displayInfo();
